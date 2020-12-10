0a1794a5cc731b708377a146fbe4cd7d3cba407c
Richard Neumann <r.neumann@homeinfo.de>
2020-12-10 11:33:14 +0100

    Added license and setup script.

---
+++ setup.py
@@ -0,0 +1,20 @@
+#! /usr/bin/env python
+"""Installation script."""
+
+from setuptools import setup
+
+setup(
+    name='hidsl',
+    version_format='{tag}',
+    setup_requires=['setuptools-git-version'],
+    author='HOMEINFO - Digitale Informationssysteme GmbH',
+    author_email='info@homeinfo.de',
+    maintainer='Richard Neumann',
+    maintainer_email='r.neumann@homeinfo.de',
+    python_requires='>=3.8',
+    packages=['hidsl'],
+    scripts=['files/hireset', 'files/hirestore', 'files/mkhidslimg'],
+    url='https://github.com/homeinfogmbh/hidsltools',
+    license='GPLv3',
+    description='Image tools for HOMEINFO Digital Signage Linux systems.'
+)
