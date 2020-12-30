64f8c60f0f6c718504330a67f8afe2a7680e3aed
Richard Neumann <r.neumann@homeinfo.de>
2020-12-30 18:38:42 +0100

    Made logger name dynamic.

--- hidsltools/logging.py
+++ hidsltools/logging.py
@@ -1,10 +1,12 @@
 """Logging facility."""
 
 from logging import getLogger
+from pathlib import Path
+from sys import argv
 
 
 __all__ = ['FORMAT', 'LOGGER']
 
 
 FORMAT = '[%(levelname)s] %(name)s: %(message)s'
-LOGGER = getLogger('restore')
+LOGGER = getLogger(Path(argv[0]).stem)
