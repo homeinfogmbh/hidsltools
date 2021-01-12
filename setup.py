c22d3f65b5815410d6b1f87bca86a7d1aef2b090
Richard Neumann <r.neumann@homeinfo.de>
2021-01-12 10:27:52 +0100

    Migrated to setuptools-scm.

--- setup.py
+++ setup.py
@@ -5,8 +5,8 @@ from setuptools import setup
 
 setup(
     name='hidsltools',
-    version_format='{tag}',
-    setup_requires=['setuptools-git-version'],
+    use_scm_version=True,
+    setup_requires=['setuptools_scm'],
     author='HOMEINFO - Digitale Informationssysteme GmbH',
     author_email='info@homeinfo.de',
     maintainer='Richard Neumann',
