e89c9ae53f877abefd749b0a49a52902e3257208
Richard Neumann <r.neumann@homeinfo.de>
2022-03-17 14:33:59 +0100

    Use passwd parsing in clean_homes()

--- hidsltools/users.py
+++ hidsltools/users.py
@@ -3,8 +3,9 @@
 from pathlib import Path
 
 from hidsltools.defaults import ROOT
-from hidsltools.functions import chroot, getent, rmsubtree
+from hidsltools.functions import chroot, rmsubtree
 from hidsltools.logging import LOGGER
+from hidsltools.passwd import get_user
 
 
 __all__ = ['clean_homes']
@@ -17,7 +18,7 @@ def clean_homes(*, root: Path = ROOT) -> None:
     """Removes all digital-signage related data."""
 
     for user in sorted(USERS):
-        home = getent(user, root=root).home
+        home = get_user(user, root=root).home
         LOGGER.debug('Cleaning home %s of user %s.', home, user)
 
         if home == ROOT:
