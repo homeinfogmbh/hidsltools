c3abb623632a1d415bc51352f2deb5ba11c14103
Richard Neumann <r.neumann@homeinfo.de>
2023-08-31 16:38:44 +0200

    Use early return in case directory does not exist

--- hidsltools/openvpn.py
+++ hidsltools/openvpn.py
@@ -15,4 +15,7 @@ CLIENTS_DIR = Path("/etc/openvpn/client")
 def delete_client_config(*, root: Path = ROOT) -> None:
     """Deletes OpenVPN clients configuration."""
 
-    rmsubtree(chroot(root, CLIENTS_DIR))
+    if not (clients_dir := chroot(root, CLIENTS_DIR)).exists():
+        return
+
+    rmsubtree(clients_dir)
